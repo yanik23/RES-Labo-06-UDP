29b263b9a706e351a00d00bbbf3891351b6c5b18
yanik23 <ylange@windowslive.com>
2021-06-17 09:43:58 +0200

    Adding functionnal payload sending with command line reading of the instrument

--- docker/image-musician/musician.js
+++ docker/image-musician/musician.js
@@ -9,34 +9,63 @@ const dgram = require('dgram');
 const socket = dgram.createSocket('udp4');
 
 
-
-// Create a measure object and serialize it to JSON
-/*const measure = new Object();
-measure.timestamp = Date.now();
-measure.location = that.location;
-measure.temperature = that.temperature;
-const payload = JSON.stringify(measure);*/
-
 // Creat a musician map and serialize it to JSON
-const instruments = ["piano", "trumpet", "flute", "violin", "drum"];
+/*const instruments = ["piano", "trumpet", "flute", "violin", "drum"];
 const sounds = ["ti-ta-ti", "pouet", "trulu", "gzi-gzi", "boum-boum"];
 
+const mapOfInstruments = new Map();
+
 if(instruments.length === sounds.length) {
-    const mapOfInstruements = new Map();
-    for (var i = 0; i < instruments; ++i) {
-        mapOfInstruements.set(instruments[i], sounds[i])
+    for (var i = 0; i < instruments.length; ++i) {
+        mapOfInstruments.set(instruments[i], sounds[i])
     }
+}*/
+
+const MAP_OF_SOUNDS = {
+    piano: 'ti-ta-ti',
+    trumpet: 'pouet',
+    flute: 'trulu',
+    violin: 'gzi-gzi',
+    drum: 'boum-boum'
+}
+function Musician(instrument) {
+    const musician = new Object();
+    musician.id = uuid.v4();
+    musician.instrument = instrument;
+    //musician.activeSince = activeSince;
+    musician.sound = MAP_OF_SOUNDS[instrument];
+
+    const payload = JSON.stringify(musician);
+
+    // Send the payload via UDP (multicast)
+    message = new Buffer(payload);
+
+    Musician.prototype.sendSound = function()
+    {
+        socket.send(message, 0, message.length, protocol.PROTOCOL_PORT, protocol.PROTOCOL_MULTICAST_ADDRESS,
+            function (err, bytes) {
+                console.log("Sent payload: " + payload + " via port " + protocol.PROTOCOL_PORT);
+
+            });
+    }
+
+    /*
+     * Let's take and send a musician every 1 seconds.
+     */
+    setInterval(this.sendSound.bind(this), 1000);
 }
 
-const musician = new Object();
-musician.uuid = uuid.v4();
-musician.instrument = instrument;
-musician.sound = sound;
 
-const payload = JSON.stringify(musician);
-// Send the payload via UDP (multicast)
-message = new Buffer(payload);
-s.send(message, 0, message.length, protocol.PROTOCOL_PORT, protocol.PROTOCOL_MULTICAST_ADDRESS,
-    function(err, bytes) {
-        console.log("Sending payload: " + payload + " via port " + socket.address().port);
-    });
+/*
+ * Let's create a new thermoter - the regular publication of measures will
+ * be initiated within the constructor
+ */
+
+var myArgs = process.argv.slice(2);
+
+if(process.argv.length != 3 || !(myArgs in MAP_OF_SOUNDS)){
+     console.log("Number of parameters invalid");
+}
+else {
+    var musician = new Musician(myArgs);
+}
